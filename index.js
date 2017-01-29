eb49e5de576f4b47ae5ce57b3da8b4b201085a29
Chris Lee <chris.lee@leankit.com>
2017-01-29 14:37:40 -0600

    added roll dependency

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 
+var roll = require('roll');
+roll = new roll();
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
